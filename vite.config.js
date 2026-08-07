182e5081303a3ec14466f62265d82d0a46e83856
Vũ Huỳnh Đức <huynhducmasterland@Mac-mini-cua-Vu.local>
2026-08-07 15:36:06 +0700

    updat3

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from "path";
 
 
 export default defineConfig ({
-    base: process.env.NODE_ENV === "production" ? "/fullstack-module1" : "/",
+    base: process.env.NODE_ENV === "production" ? "/fullstack-module1/" : "/",
     root: "src",
     publicDir: "../public",
     build: {
