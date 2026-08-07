70ec844a220ed67186dcefac024e9d0dd6ccc9de
Vũ Huỳnh Đức <huynhducmasterland@Mac-mini-cua-Vu.local>
2026-08-07 15:04:29 +0700

    update2

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from "path";
 
 
 export default defineConfig ({
-    base: "/fullstack-f8-zoom/",
+    base: process.env.NODE_ENV === "production" ? "/fullstack-module1" : "/",
     root: "src",
     publicDir: "../public",
     build: {
