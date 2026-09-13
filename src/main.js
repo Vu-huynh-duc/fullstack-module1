7e7bd675fb1901b703fbda68b30c882895f84f1d
Vũ Huỳnh Đức <huynhducmasterland@Mac-mini-cua-Vu.local>
2026-09-13 18:31:43 +0700

    update fetch dữ liệu từ json-server và ứng dụng todo list

--- src/main.js
+++ src/main.js
@@ -1,52 +1,123 @@
 import "./scss/main.scss";
-
-
 const $ = document.querySelector.bind(document)
 const $$ = document.querySelectorAll.bind(document)
 
-const productsList = $(".productsList")
-const productsListRow = productsList.querySelector(".row")
+const formTodo = $(".formTodo")
+const inputTodo = $(".inputTodo")
+const tasksList = $(".tasksList")
 
-const worker1 = new Worker("./worker.js")
 
-async function start() {
-    const response = await fetch("https://dummyjson.com/products")
-    
-    const data =  await response.json()
+formTodo.onsubmit = (e) => {
+  e.preventDefault()
+  const newTask = {
+    name: inputTodo.value,
+  }
+  
+ fetch("http://localhost:3000/tasks",{
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json"
+      },
+      body: JSON.stringify(newTask)
 
-    worker1.postMessage(data.products)
-    
-}
-start()
-
-worker1.onmessage = (e) => {
-      const products = e.data
-      const fragment = document.createDocumentFragment()
-
-      products.forEach(item => {
-            const productCol = document.createElement('div')            
-            productCol.classList.add("col")            
-
-            const product = document.createElement('div')
-            product.innerText = item.title
-            product.classList.add("product")
-
-            product.dataset.id = item.id
-
-            
-            productCol.appendChild(product)
-            fragment.appendChild(productCol)
-        })
-      productsListRow.appendChild(fragment)
+      })
+      .then(res=> res.json())
+      .then(task => {
+        createLiTask(task)
+      })    
 }
 
+tasksList.onclick = (e) => {
+  const deleteBtn = e.target.closest(".deleteTask")
+  const updateBtn = e.target.closest(".updateTask")
+  
+  if(deleteBtn) {
+    fetch(`http://localhost:3000/tasks/${deleteBtn.dataset.id}`,{
+      method: "DELETE"
+    })
+    .then(res=> res.json())
+    .then(task => {
+        deleteBtn.parentElement.remove()
+    })
+  }
 
-productsListRow.onclick = (e) => {
-  if(e.target.closest(".product")) {
-    const product = e.target.closest(".product")
-    window.location.href = `details.html?id=${product.dataset.id}`
+  if(updateBtn) {
+
+    const taskLi = updateBtn.closest(".task-item")
+    const taskName = taskLi.querySelector(".task-name")
+
+    const input = document.createElement("input")
+    input.value = taskName.textContent
+
+    taskName.replaceWith(input)
+
+    input.focus()
+
+   
+
   }
 }
 
+tasksList.onkeydown = (e) => {
+  if (e.key !== "Enter") return
+  const input = e.target
+  if (!input.matches(".task-item input")) return
+  const taskLi = input.closest(".task-item")
+  const updateBtn = taskLi.querySelector(".updateTask")
 
+   fetch(`http://localhost:3000/tasks/${updateBtn.dataset.id}`,{
+      method: "PATCH",
+      headers: {
+          "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        name: input.value
+      })
+    })
+    .then(res=> res.json())
+    .then(task => {
+        const taskName = document.createElement("span")
+        taskName.classList.add("task-name")
+        taskName.textContent = task.name
 
+        input.replaceWith(taskName)
+      })
+  
+}
+
+function createLiTask(task) {
+  const taskLiElement = document.createElement("li")
+  taskLiElement.classList.add("task-item")
+
+  const taskName = document.createElement("span")
+  taskName.classList.add("task-name")
+  taskName.textContent = task.name
+
+  const updateBtn = document.createElement("button")
+  updateBtn.classList.add("updateTask")
+  updateBtn.textContent = "Update"
+  updateBtn.dataset.id = task.id
+
+  const deleteBtn = document.createElement("button")
+  deleteBtn.classList.add("deleteTask")
+  deleteBtn.textContent = "X"
+  deleteBtn.dataset.id = task.id
+
+  taskLiElement.append(taskName, updateBtn, deleteBtn)
+
+  tasksList.prepend(taskLiElement)
+  
+}
+
+function renderTasks(tasks) {
+  tasks.forEach(task => {
+    createLiTask(task)
+  });
+}
+
+async function start() {
+  const res = await fetch("http://localhost:3000/tasks")
+  const tasks = await res.json()
+  renderTasks(tasks)
+} 
+start()
